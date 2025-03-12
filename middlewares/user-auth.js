3ff443940c96c4bc250f9141c1b137dc8896c4e6
Cao007 <1114685490@qq.com>
2025-03-12 16:48:59 +0800

    feat: 后台JWT规范

--- middlewares/user-auth.js
+++ middlewares/user-auth.js
@@ -5,7 +5,7 @@ const { failure } = require('../utils/responses')
 module.exports = async (req, res, next) => {
   try {
     // 判断 Token 是否存在
-    const token = req.headers.token || req.query.token
+    const token = req.headers.authorization?.split(' ')[1] || req.query.token
     if (!token) {
       throw new Unauthorized('当前接口需要认证才能访问。')
     }
